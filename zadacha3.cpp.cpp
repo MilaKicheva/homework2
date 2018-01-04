20c777ea6d7fef324a847574f70e7bb9fd08133b
Mila Kicheva <32715593+MilaKicheva@users.noreply.github.com>
2018-01-04 20:12:38 +0200

    Add files via upload

---
+++ zadacha3.cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+const int ROWS = 5;
+const int COLS = 4;
+
+int main()
+{
+	int arrFirst[ROWS][COLS] = { { 11, 17, 4, 10 },
+								{ 29, 4, 18, 18 },
+								{ 22, 14, 5, 5 },
+								{ 1, 27, 1, 11 },
+								{ 25, 2, 27, 6 } };
+int arrSecond[ROWS][COLS] = { {1,1,1,1},
+								{1,1,1,1},
+								{1,1,1,1},
+								{1,1,1,1},
+								{1,1,1,1} };
+int arrThird[ROWS][COLS];
